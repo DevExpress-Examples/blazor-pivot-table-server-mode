64a50c7c094fd3695483628590fa0c11f0c39c45
Anton Shapovalov <anton.shapovalov@devexpress.com>
2026-07-31 13:32:49 +0400

    server mode implemented

--- CS/PivotTableBindToData/Data/PivotRow.cs
+++ CS/PivotTableBindToData/Data/PivotRow.cs
@@ -1,5 +1,7 @@
 ﻿namespace PivotTableBindToData.Data {
     public class PivotRow {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
         public string CategoryName { get; set; }
         public string ProductName { get; set; }
         public decimal? UnitPrice { get; set; }
